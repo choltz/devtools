db5d7f5f6cce5fad84ce17aa99cfd42aec5cdd9a
Chris Holtz <choltz@gmail.com>
2014-03-04 18:24:49 -0500

    Started refactoring db pull class - moved remote file interaction to it's own file.

--- lib/db_pull/db_pull.rb
+++ lib/db_pull/db_pull.rb
@@ -7,26 +7,20 @@ require_relative "../remote"
 class Pull
   def initialize
     # Connect to S3
-    @s3_connection = Remote.new 'config/s3.yml'
-    @bucket        = @s3_connection.bucket
+    @remote = Remote.new 'config/s3.yml'
+    @bucket = @remote.bucket
   end
 
   def call
-    files       = @s3_connection.directories.get( @bucket ).files
-    latest_file = files.sort{|a,b| b.last_modified <=> a.last_modified}.first
-    file_name   = latest_file.key.scan(/[^\/$]+$/).first
-
-    # configure the progress bar
     progress          = ProgressBar.new 'Downloading', 100
     progress.bar_mark = '='
 
-    # Stream the S3 file and write locally in chunks
-    File.open("backups/#{file_name}", 'w') do |local_file|
-      files.get(latest_file.key) do |chunk, remaining, total|
+    # Stream the remote file and write locally in chunks
+    File.open("backups/#{@remote.latest_file_name}", 'w') do |local_file|
+      @remote.read_latest_file do |chunk, remaining, total|
         local_file.write chunk
 
         percent = (100 - (remaining.to_f / total.to_f * 100))
-
         progress.set(percent)
       end
     end
--- lib/remote.rb
+++ lib/remote.rb
@@ -6,14 +6,39 @@ class Remote < OpenStruct
 
   def initialize(path)
     config      = YamlConfig.new path
+
     @connection = Fog::Storage.new(
       :provider                 => 'AWS',
       :aws_access_key_id        => config.key,
       :aws_secret_access_key    => config.secret
-      )
+    )
 
     # Add attributes for key/values found in the config file
-
     super(YAML.load_file path)
   end
+
+  def file_name(file)
+    file.key.scan(/[^\/$]+$/).first
+  end
+
+  def latest_file
+    file_list.sort{|a,b| b.last_modified <=> a.last_modified}.first
+  end
+
+  def latest_file_name
+    file_name latest_file
+  end
+
+  def read_latest_file
+    file_list.get(latest_file.key) do |chunk, remaining, total|
+      yield chunk, remaining, total
+    end
+  end
+
+  private
+
+  def file_list
+    @connection.directories.get( bucket ).files
+  end
+
 end
